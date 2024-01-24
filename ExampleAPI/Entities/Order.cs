85fbd9cc677fcbf62f4cae8af70e59aec87139f6
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-01-25 02:54:53 +0300

    Add project files.

---
+++ ExampleAPI/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using ExampleAPI.Core;
+
+namespace ExampleAPI.Entities
+{
+    public class Order:Entity<Guid>
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public virtual ICollection<Stock> Stocks { get; set; }
+
+        public Order()
+        {
+            Stocks = new HashSet<Stock>();
+        }
+    }
+}
